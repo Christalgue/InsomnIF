60c8d4edc0d6e99344361b77f1f153656a8c44fd
MainHanzo <33153091+MainHanzo@users.noreply.github.com>
2018-11-16 10:52:45 +0100

    add head.js

---
+++ src/assets/templates/head.js
@@ -0,0 +1,25 @@
+document.write('\
+<!DOCTYPE html>\
+<!--\
+To change this license header, choose License Headers in Project Properties.\
+To change this template file, choose Tools | Templates\
+and open the template in the editor.\
+-->\
+\
+<html>\
+    <div class="h1 text-center bg-primary">Insomn\'IF</div>\
+    <div class="container">\
+        <div class="row align-middle">\
+            <div class="col-4 text-center pl-0 align-middle">\
+                <button class="border-0 bg-white">Récap Global</button>\
+            </div>\
+            <div class="col-4 text-center pl-0 pr-0 align-middle">\
+                <button class="border-0 bg-white">Récap Semaine</button>\
+            </div>\
+            <div class="col-4 text-center pr-0 align-middle">\
+                <button class="border-0 bg-white">Archétype</button>\
+            </div>\
+        </div>\
+    </div>\
+</html>\
+');
