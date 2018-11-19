4b99c6fa7b9f032f8ab72cc9769ea3105bb6d219
yanghua <yhua@yanghuatekiMacBook-Air.local>
2018-11-19 15:11:39 +0100

    3 main buttons added

---
+++ src/assets/templates/headerGB.js
@@ -0,0 +1,40 @@
+document.write('\
+<!DOCTYPE html>\
+<!--\
+To change this license header, choose License Headers in Project Properties.\
+To change this template file, choose Tools | Templates\
+and open the template in the editor.\
+-->\
+\
+<html>\
+    <head>\
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">\
+        <title>Header</title>\
+        <meta charset="windows-1252">\
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    </head>\
+    <body>\
+      <div class="row" style="background-color:#d3e1af">\
+        <div class="col-3 text-center" style=" padding-top:10px">\
+          <a href="header.html"><img src="../img/button_back.png"  width=30px height=30px style="" /></a>\
+        </div>\
+        <div class="h1 col-6  text-center"  style="">\
+          Insomn\'IF\
+        </div>\
+      </div>\
+        <div class="container">\
+            <div class="row align-middle">\
+                <div class="col-4 text-center pl-0 align-middle" >\
+                    <button onclick="window.location=\'file:///Users/yhua/Desktop/IHM/insomnif/src/assets/templates/RecapGlobal.html\';" class="border-right-0 border-top-0 border-left-0 bg-white" style="border-bottom: 2px solid grey;" >Recap Global</button>\
+                </div>\
+                <div class="col-4 text-center pl-0 pr-0 align-middle">\
+                    <button  onclick="window.location=\'file:///Users/yhua/Desktop/IHM/insomnif/src/assets/templates/RecapSemaine.html\';"class="border-right-0 border-top-0 border-left-0 bg-white" style="border-bottom: 2px solid white;">Recap Semaine</button>\
+                </div>\
+                <div class="col-4 text-center pr-0 align-middle">\
+                    <button  onclick="window.location=\'file:///Users/yhua/Desktop/IHM/insomnif/src/assets/templates/ListeArchetypes.html\';"class="border-right-0 border-top-0 border-left-0 bg-white" style="border-bottom: 2px solid white;">Archetype</button>\
+                </div>\
+            </div>\
+        </div>\
+    </body>\
+</html>\
+');
