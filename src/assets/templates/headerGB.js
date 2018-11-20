13a5ed94d1813287a49875c9d2e773bbc04336b1
Christophe GIBERT <christophe.gibert69@gmail.com>
2018-11-20 18:21:49 +0100

    Add real link for the return button on the RecapGlobal and ListeArchetype. Little adjustements on ListeArchetype

--- src/assets/templates/headerGB.js
+++ src/assets/templates/headerGB.js
@@ -4,7 +4,7 @@ document.write(`
     <body>
       <div class="row bg-primary">
         <div class="col-3 text-center" style=" padding-top:10px">
-          <a href="#"><img src="../img/button_back.png" width=30px height=30px style="" /></a>
+          <a href="RecapSemaine.html"><img src="../img/button_back.png" width=30px height=30px style="" /></a>
         </div>
         <div class="h1 col-6 text-center">
           Insomn'IF
