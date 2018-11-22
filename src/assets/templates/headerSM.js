abde90d0b11461e30a456eb9c6018d2392bf1ccd
IsmailZemmouri <36330368+IsmailZemmouri@users.noreply.github.com>
2018-11-22 17:12:11 +0100

    change background color for stats.html header

--- src/assets/templates/headerSM.js
+++ src/assets/templates/headerSM.js
@@ -3,7 +3,7 @@ document.write(`
     <body>
 		<div class="row bg-primary">
 			<div class="col-3 text-center" style=" padding-top:10px">
-				<a href="#"><img src="../img/button_back.png" width=30px height=30px style="" /></a>
+				<!--<a href="#"><img src="../img/button_back.png" width=30px height=30px style="" /></a>-->
 			</div>
 			<div class="h1 col-6 text-center">
 				Insomn'IF
