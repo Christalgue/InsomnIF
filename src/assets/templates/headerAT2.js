c919a2725f4d48d73080764b743e9a9ddde96790
Christophe GIBERT <christophe.gibert69@gmail.com>
2018-11-22 21:15:30 +0100

    Modification and add files for the demonstration

---
+++ src/assets/templates/headerAT2.js
@@ -0,0 +1,28 @@
+document.write(`
+<!DOCTYPE html>
+<html>
+    <body>
+      <div class="row bg-primary">
+        <div class="col-3 text-center" style=" padding-top:10px">
+          <a href="RecapSemaineModifie2.html"><img src="../img/button_back.png" width=30px height=30px style="" /></a>
+        </div>
+        <div class="h1 col-6 text-center">
+          Insomn'IF
+        </div>
+      </div>
+        <div class="container">
+            <div class="row align-middle">
+                <div class="col-4 text-center pl-0 align-middle" >
+                    <button onclick="window.location='RecapGlobal.html'" class="border-0 bg-white head-button-size w-100">Recap Global</button>
+                </div>
+                <div class="col-4 text-center pl-0 pr-0 align-middle">
+                    <button  onclick="window.location='RecapSemaine.html'"class="border-0 bg-white head-button-size w-100">Recap Semaine</button>
+                </div>
+                <div class="col-4 text-center pr-0 align-middle">
+                    <button  onclick="window.location='ListeArchetypes.html'"class="border-0 bg-secondary head-button-size w-100">Archetype</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+`);
--- src/assets/templates/headerSJ.js
+++ src/assets/templates/headerSJ.js
@@ -3,25 +3,12 @@ document.write(`
     <body>
 		<div class="row bg-primary">
 			<div class="col-3 text-center" style=" padding-top:10px">
-				 <a href="RecapSemaine.html"><img src="../img/button_back.png" width=30px height=30px style="" /></a> 
+				 <a href="RecapSemaineModifie.html"><img src="../img/button_back.png" width=30px height=30px style="" /></a> 
 			</div>
 			<div class="h1 col-6 text-center">
 				Insomn'IF
 			</div>
 		</div>
-        <div class="container">
-            <div class="row align-middle">
-                <div class="col-4 text-center pl-0 align-middle" >
-                    <button onclick="window.location='RecapGlobal.html'" class="border-0 bg-white head-button-size w-100">Recap Global</button>
-                </div>
-                <div class="col-4 text-center pl-0 pr-0 align-middle">
-                    <button  onclick="window.location='RecapSemaine.html'"class="border-0 bg-secondary head-button-size w-100">Recap Semaine</button>
-                </div>
-                <div class="col-4 text-center pr-0 align-middle">
-                    <button  onclick="window.location='ListeArchetypes.html'"class="border-0 bg-white head-button-size w-100">Archetype</button>
-                </div>
-            </div>
-        </div>
     </body>
 </html>
 `);
